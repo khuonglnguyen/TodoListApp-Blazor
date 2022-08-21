60c03cecf112d8cad4e80d72b671193ac0173f7f
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-21 20:56:57 +0700

    Added CRUD Task

--- TodoListApp.API/Controllers/TasksController.cs
+++ TodoListApp.API/Controllers/TasksController.cs
@@ -15,12 +15,53 @@ namespace TodoListApp.API.Controllers
             _taskRepository = taskRepository;
         }
 
+        //api/tasks
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var tasks = await _taskRepository.GetTaskList();
             return Ok(tasks);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tasks = await _taskRepository.Create(task);
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, tasks);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(Guid id, Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var taskFromDb = await _taskRepository.GetById(id);
+
+            if (taskFromDb == null)
+            {
+                return NotFound($"{id} is not found");
+            }
+
+            taskFromDb.Name = task.Name;
+            var tasks = await _taskRepository.Update(task);
+
+            return Ok(tasks);
+        }
+
+
+        //api/tasks/xxxx
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var task = await _taskRepository.GetById(id);
+            if (task == null) return NotFound($"{id} is not found");
+            return Ok(task);
         }
     }
 }
