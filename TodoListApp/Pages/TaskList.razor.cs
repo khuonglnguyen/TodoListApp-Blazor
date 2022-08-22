1cc7596d36b0a2a46dec0c9a3d317202ff908f11
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-22 18:21:51 +0700

    Search Component

--- TodoListApp/Pages/TaskList.razor.cs
+++ TodoListApp/Pages/TaskList.razor.cs
@@ -8,22 +8,20 @@ namespace TodoListApp.Pages
     public partial class TaskList
     {
         [Inject] private ITaskApiClient TaskApiClient { set; get; }
-        [Inject] private IUserApiClient UserApiClient { set; get; }
 
         private List<TaskDto> Tasks;
 
-        private List<AssigneeDto> Assignees;
 
         private TaskListSearch TaskListSearch = new TaskListSearch();
 
         protected override async Task OnInitializedAsync()
         {
             Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
-            Assignees = await UserApiClient.GetAssignees();
         }
 
-        private async Task SearchForm(EditContext context)
+        public async Task SearchTask(TaskListSearch taskListSearch)
         {
+            TaskListSearch = taskListSearch;
             Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
         }
     }
