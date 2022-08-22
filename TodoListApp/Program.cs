66190efc9305b2cb7ffb30527184d0d0f8bad40e
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-08-22 16:17:22 +0700

    Added appsetting file

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BackendApiUrl"))});
 builder.Services.AddTransient<ITaskApiClient, TaskApiClient>();
 builder.Services.AddTransient<IUserApiClient, UserApiClient>();
 builder.Services.AddBlazoredToast();
